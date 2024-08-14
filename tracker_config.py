43564fed62b66042df00050b0f5bdf74f53e72d8
Andrew Francisco <unipolarity@polarityai.com>
2024-08-13 23:24:00 -0400

    Add files via upload

---
+++ tracker_config.py
@@ -0,0 +1,17 @@
+# Example configuration settings
+
+# QSettings configurations
+ORGANIZATION_NAME = "polarityAI"
+APPLICATION_NAME = "bsl8_1"
+# logger_setup
+LOG_FILE = 'BSLM11.log'
+PRINGLES = 'BSLM11'  # lol the directory made/placed
+DATEFORMAT = '%d-%b-%y %I:%M:%S %p'  # this is how you want it from now on lolol ok?
+FILE_MODE = 'w'
+# database
+# DB_NAME = 'theDBofTracksAugust8th.db'
+DB_NAME = 'theDBofTracksAugust8th.db'
+
+
+
+
